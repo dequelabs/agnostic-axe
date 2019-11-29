f5d9c6092dd40e5b9f5f997c5976ea36a0c68011
Juliette Pretot <m@juliette.sh>
2019-11-29 15:04:21 +0100

    use a queue of pending audits to run on idle

--- AxeObserver.js
+++ AxeObserver.js
@@ -1,18 +1,5 @@
 import axeCore from 'axe-core'
-import PQueue from 'p-queue'
-
-// If requestIdleCallback is not supported, we fallback to setTimeout
-// Ref: https://developers.google.com/web/updates/2015/08/using-requestidlecallback
-const requestIdleCallback =
-  window.requestIdleCallback ||
-  function(callback) {
-    setTimeout(callback, 1)
-  }
-
-// axe-core cannot be run concurrently. There are no plans to implement this
-// functionality or a queue. Hence we use PQueue to queue calls ourselves.
-// Ref: https://github.com/storybookjs/storybook/pull/4086
-const axeQueue = new PQueue({ concurrency: 1 })
+import AuditQueue from './AuditQueue.js'
 
 // The AxeObserver class takes a violationsCallback, which is invoked with an
 // array of observed violations.
@@ -45,14 +32,17 @@ export default class AxeObserver {
 
     this.observe = this.observe.bind(this)
     this.disconnect = this.disconnect.bind(this)
-    this._scheduleAudit = this._scheduleAudit.bind(this)
+    this._auditNode = this._auditNode.bind(this)
 
     this._alreadyReportedIncidents = new Set()
     this._mutationObserver = new window.MutationObserver(mutationRecords => {
       mutationRecords.forEach(mutationRecord => {
-        this._scheduleAudit(mutationRecord.target)
+        this._auditNode(mutationRecord.target)
       })
     })
+
+    // AuditQueue sequentially runs audits when the browser is idle.
+    this._auditQueue = new AuditQueue()
 
     // Allow for registering plugins etc
     if (typeof axeInstanceCallback === 'function') {
@@ -73,27 +63,17 @@ export default class AxeObserver {
     })
 
     // run initial audit on the whole targetNode
-    this._scheduleAudit(targetNode)
+    this._auditNode(targetNode)
   }
   disconnect() {
     this.mutationObserver.disconnect()
   }
-  async _scheduleAudit(node) {
-    const response = await axeQueue.add(
-      () =>
-        new Promise(resolve => {
-          requestIdleCallback(
-            () => {
-              // Since audits are scheduled asynchronously, it can happen that
-              // the node is no longer connected. We cannot analyze it then.
-              node.isConnected ? axeCore.run(node).then(resolve) : resolve(null)
-            },
-            // Time after which an audit will be performed, even if it may
-            // negatively affect performance.
-            { timeout: 1000 }
-          )
-        })
-    )
+  async _auditNode(node) {
+    const response = await this._auditQueue.run(async () => {
+      // Since audits are scheduled asynchronously, it can happen that
+      // the node is no longer connected. We cannot analyze it then.
+      return node.isConnected ? axeCore.run(node) : null
+    })
 
     if (!response) return
 
