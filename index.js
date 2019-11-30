df5568f5805eac5de44edd6d1454a9389b54407a
Juliette Pretot <hi@juliette.sh>
2019-11-30 23:01:59 +0100

    rename files to mjs for clarity

--- index.js
+++
@@ -1,5 +0,0 @@
-import _AxeObserver from './AxeObserver.js'
-import _logViolations from './logViolations.js'
-
-export const AxeObserver = _AxeObserver
-export const logViolations = _logViolations
---
+++ index.mjs
@@ -0,0 +1,5 @@
+import _AxeObserver from './AxeObserver.mjs'
+import _logViolations from './logViolations.mjs'
+
+export const AxeObserver = _AxeObserver
+export const logViolations = _logViolations
